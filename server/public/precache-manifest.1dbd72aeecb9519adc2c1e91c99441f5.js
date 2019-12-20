6aa200c5e9ec6f4d3fe6bc351f7b54f729928095
Rebecca Wood <rebecca@Rebeccas-MacBook-Air.local>
2019-12-20 13:07:07 +0100

    Deploying

---
+++ server/public/precache-manifest.1dbd72aeecb9519adc2c1e91c99441f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f360eadcd0f921cc350ac596b6f93a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f40eaff3fef87233c3ce",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "408122062f50bb7bdd14",
+    "url": "/static/css/main.361eda27.chunk.css"
+  },
+  {
+    "revision": "f40eaff3fef87233c3ce",
+    "url": "/static/js/2.f3cf0e4b.chunk.js"
+  },
+  {
+    "revision": "72513fbf30db4ceb5f37069eaa977c04",
+    "url": "/static/js/2.f3cf0e4b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "408122062f50bb7bdd14",
+    "url": "/static/js/main.acf2b163.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
